b6335ad1e0e5485b78e27d2a40bd39431cc67bde
Martin <120989823+MartinRekalde@users.noreply.github.com>
2023-11-27 12:14:47 +0000

    inicio

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 10:08:36 by codespace         #+#    #+#             */
+/*   Updated: 2023/11/23 10:08:37 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+char *get_next_line(int fd);
+
+#endif
